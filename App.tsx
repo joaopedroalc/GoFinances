c4074d0502f2e0b1d9ec5ba0660fff459d7a148e
João Pedro de Alcântara Lima <joaopedro.ufma@gmail.com>
2022-03-15 13:44:39 -0300

    app Routes

--- App.tsx
+++ App.tsx
@@ -16,6 +16,8 @@ import theme from './src/global/styles/theme';
 
 //AppLoading
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +32,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
